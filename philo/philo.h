3a778f52bc00c4e5871df63c48afbf76416e5f43
dvavryn <vavryn.dominic@outlook.com>
2025-06-25 22:32:29 +0200

    changes

---
+++ philo/philo.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 22:05:44 by dvavryn           #+#    #+#             */
+/*   Updated: 2025/06/25 22:15:41 by dvavryn          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+// memset
+# include <string.h>
+//--------------------
+// printf
+# include <stdio.h>
+//--------------------
+// malloc // free
+# include <stdlib.h>
+//--------------------
+// write // usleep
+# include <unistd.h>
+//--------------------
+// gettimeofday
+# include <sys/time.h>
+//--------------------
+// pthread_create // pthread_detach 
+// pthread_join // pthread_mutex_init
+// pthread_mutex_lock // pthread_mutex_unlock
+// pthread_mutex_destroy
+# include <pthread.h>
+
+#endif
