be40b62e7b4e5ddd8a9e0592cd51a8570e412322
Dominic Vavryn <dvavryn@c3r5p1.42vienna.com>
2025-09-18 19:06:26 +0200

    changes

--- src/routine_philo_odd.c
+++ src/routine_philo_odd.c
@@ -6,7 +6,7 @@
 /*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 15:50:02 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/09/18 15:50:38 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/09/18 19:06:14 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,18 @@
 
 void	odd_philo_eat_even(t_philo *philo)
 {
+	// 5 600 200 200 should die!!!
 	pthread_mutex_lock(&philo->fork_one->mtx);
 	safe_print(philo, "has taken a fork");
 	pthread_mutex_lock(&philo->fork_two->mtx);
 	safe_print(philo, "has taken a fork");
 	safe_print(philo, "is eating");
-	ft_usleep(philo->data, philo->data->time_eat);
 	pthread_mutex_lock(&philo->mtx_meal.mtx);
 	philo->meals_eaten++;
 	if (philo->meals_eaten == philo->data->num_meals)
 		philo->full = 1;
 	philo->last_meal = get_time_ms();
+	ft_usleep(philo->data, philo->data->time_eat);
 	pthread_mutex_unlock(&philo->mtx_meal.mtx);
 	pthread_mutex_unlock(&philo->fork_two->mtx);
 	pthread_mutex_unlock(&philo->fork_one->mtx);
@@ -37,12 +38,12 @@ void	odd_philo_eat_odd(t_philo *philo)
 	pthread_mutex_lock(&philo->fork_one->mtx);
 	safe_print(philo, "has taken a fork");
 	safe_print(philo, "is eating");
-	ft_usleep(philo->data, philo->data->time_eat);
 	pthread_mutex_lock(&philo->mtx_meal.mtx);
 	philo->meals_eaten++;
 	if (philo->meals_eaten == philo->data->num_meals)
 		philo->full = 1;
 	philo->last_meal = get_time_ms();
+	ft_usleep(philo->data, philo->data->time_eat);
 	pthread_mutex_unlock(&philo->mtx_meal.mtx);
 	pthread_mutex_unlock(&philo->fork_one->mtx);
 	pthread_mutex_unlock(&philo->fork_two->mtx);
